83b636e5d03d471ce804e52ef1c0883be743a51f
Maduabughichi Achilefu <aachilefu@yahoo.com>
2019-11-15 12:09:02 +0100

    Room With Kotlin

---
+++ app/src/main/java/com/dodemy/android/roomwithkotlin/WordRepository.kt
@@ -0,0 +1,18 @@
+package com.dodemy.android.roomwithkotlin
+
+import androidx.lifecycle.LiveData
+
+/**
+ * Abstracted Repository as promoted by the Architecture Guide.
+ * https://developer.android.com/topic/libraries/architecture/guide.html
+ */
+class WordRepository(private val wordDao: WordDao) {
+
+    // Room executes all queries on a separate thread.
+    // Observed LiveData will notify the observer when the data has changed.
+    val allWords: LiveData<List<Word>> = wordDao.getAlphabetizedWords()
+
+    suspend fun insert(word: Word) {
+        wordDao.insert(word)
+    }
+}
